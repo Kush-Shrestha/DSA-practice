2345e01b4d5705ea22ee3c89e42a1b3c16a5dceb
Kushal Shrestha <shresthakushal097@gmail.com>
2025-12-16 10:25:50 +0545

    DSA using C++

---
+++ day10(indexsearch).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num[]={1,3,5,7,11,9};
+    int n;
+    int index= -1;
+    cout<<"Enter any number:"<<endl;
+    cin>>n;
+    int size=sizeof(num)/sizeof(num[0]);
+
+for(int i=0;i<size;i++) {
+    if (num[i]==n) {
+    index=i;
+    }
+}
+if(index !=-1){
+ cout<<"Number found at index:"<<index<<endl;
+}else{
+    cout<<"Number not found"<<endl;
+}
+return 0;
+}
