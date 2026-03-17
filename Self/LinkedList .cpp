98fe3dc037e31e47a4a884e5e30ebbba64caceac
Kushal Shrestha <shresthakushal097@gmail.com>
2026-03-17 13:20:08 +0545

    Preparing

--- Self/LinkedList .cpp
+++ Self/LinkedList .cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
- 
+
 class Node{
     public:
        int data;
        Node* next;
     public:
-       Node(int data1,Node* next1){
+       Node(int data1, Node* next1){
         data=data1;
         next=next1;
        }
@@ -16,10 +16,25 @@ class Node{
         next=NULL;
        }
 };
+
+Node* convertarr2LL(int arr[], int size){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<size; i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
 int main(){
     int arr[]={2,3,4,5,6};
+    int size=sizeof(arr)/sizeof(arr[0]);
     Node* y= new Node(arr[0]);
-    cout<<y;// for address
-    cout<<y->data; // for data and its output will be 2
+    cout<<y<<endl;        // for address
+    cout<<y->data<<endl;  // for data and its output will be 2
+    Node* head = convertarr2LL(arr, size);
+    cout<<head->data;
     return 0;
 }
